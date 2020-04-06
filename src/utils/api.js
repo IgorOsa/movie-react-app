876a1ff119642ddb1c1c5f323041179483bec0bd
Igor Osadchyi <igor@webio.net.ua>
2020-04-06 19:50:02 +0300

    readme mods

---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "79b8d6f4cd09f385e53d847858d78a4a";
+
+export const API_KEY_4 =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OWI4ZDZmNGNkMDlmMzg1ZTUzZDg0Nzg1OGQ3OGE0YSIsInN1YiI6IjVlN2I5ZWEwYWIxYmM3MDAxNWMxZjcwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h2kTlYYKlPlDT4bMP6lg7Puftd0n1aM8aR9_uqIGIw4";
