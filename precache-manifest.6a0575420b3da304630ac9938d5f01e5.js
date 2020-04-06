4ccb92f0844473b8c90ace2b6d36c28cb95f6505
Igor Osadchyi <igor@webio.net.ua>
2020-04-06 20:15:23 +0300

    Updates

---
+++ precache-manifest.6a0575420b3da304630ac9938d5f01e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb49a92a10e0640e0a0d651ec501fc5",
+    "url": "/Movie-React-App/index.html"
+  },
+  {
+    "revision": "9f40750af81a9a64ac85",
+    "url": "/Movie-React-App/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fb5c5e9790b54af7471e",
+    "url": "/Movie-React-App/static/css/main.51b8b145.chunk.css"
+  },
+  {
+    "revision": "9f40750af81a9a64ac85",
+    "url": "/Movie-React-App/static/js/2.280ade7e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Movie-React-App/static/js/2.280ade7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5c5e9790b54af7471e",
+    "url": "/Movie-React-App/static/js/main.81398877.chunk.js"
+  },
+  {
+    "revision": "687e28c47332f801d4b4",
+    "url": "/Movie-React-App/static/js/runtime-main.4aa8897d.js"
+  }
+]);
